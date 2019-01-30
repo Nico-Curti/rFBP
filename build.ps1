eb11fa19366d1709578779d56e2aa02fdd6d5463
Nico-Curti <nico.curti2@unibo.it>
2019-01-30 12:03:35 +0100

    fix build type

--- build.ps1
+++ build.ps1
@@ -4,7 +4,7 @@ Remove-Item build -Force -Recurse -ErrorAction SilentlyContinue
 New-Item -Path .\build -ItemType directory -Force
 Set-Location build
 
-cmake -G "Visual Studio 15 2017 Win64" "-DCMAKE_BUILD_TYPE=Release" ..
-cmake --build . --config Release
+cmake -G "Visual Studio 15 2017 Win64" "-DOMP=OFF" "-DCMAKE_BUILD_TYPE=Release" ..
+cmake --build . --config Release --target install
 
 Set-Location ..
--- build.sh
+++ build.sh
@@ -7,13 +7,16 @@ if [[ "$OSTYPE" == "darwin"* ]]; then
   #export CXX="/usr/local/Cellar/llvm/7.0.0_1/bin/clang++"
   #export OPENMP_LIBRARIES="/usr/local/Cellar/llvm/7.0.0_1/lib"
   #export OPENMP_INCLUDES="/usr/local/Cellar/llvm/7.0.0_1/include"
+else
+  export CC="/usr/bin/gcc-8"
+  export CXX="/usr/bin/g++-8"
 fi
 
-#rm -rf build
+rm -rf build
 mkdir -p build
 cd build
 
 ##sudo apt-get install ninja-build
-cmake -G "Ninja" "-DCMAKE_BUILD_TYPE=Release" ..
-cmake --build .
+cmake -G "Ninja" "-DOMP=ON" "-DCMAKE_BUILD_TYPE=Release" ..
+make -j4 install
 cd ..
