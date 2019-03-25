3b0f9d28bd9a39a0fa76cca5a0688a78c1d1df6d
Nico-Curti <nico.curti2@unibo.it>
2019-03-25 14:44:41 +0100

    fix bash script

--- build.sh
+++ build.sh
@@ -1,22 +1,22 @@
-#!usr/bin/env bash
+#!/bin/bash
 
 red=`tput setaf 1`
 reset=`tput sgr0`
 
 # $1 debug or release
 build_type=$1
-compiler=echo ${CXX##*/}
+compiler=$(echo ${CXX##*/})
 number_of_build_workers=8
 
 
-if [[ $compiler == *"g++"* ]]; then
+if [[ "$compiler" == *"g++"* ]]; then
   allow_omp=ON;
 else
   allow_omp=OFF;
-fi;
+fi
 
 
-if [ $build_type == "" ];
+if [ "$build_type" == "" ]; then
   rm -rf build_release
   mkdir -p build_release
   cd build_release
@@ -33,7 +33,7 @@ if [ $build_type == "" ];
   cd build_release && cmake --build . --target install -- -j${number_of_build_workers} && cd ..
 
 
-elif [ $build_type == "Release" ] || [ $build_type == "release" ]; then
+elif [ "$build_type" == "Release" ] || [ "$build_type" == "release" ]; then
   echo ${green}"Building Release project"${reset}
   build_type=Release
   rm -rf build_release
@@ -44,7 +44,7 @@ elif [ $build_type == "Release" ] || [ $build_type == "release" ]; then
   cmake --build . --target install -- -j8
   cd ..
 
-elif [ $build_type == "Debug" ] || [ $build_type == "debug" ]; then
+elif [ "$build_type" == "Debug" ] || [ "$build_type" == "debug" ]; then
   echo ${green}"Building Debug project"${reset}
   build_type=Debug
   rm -rf build_debug
@@ -57,7 +57,7 @@ elif [ $build_type == "Debug" ] || [ $build_type == "debug" ]; then
 
 else
   echo ${red}"Unknown build type - Allowed only [Debug, Release]"${reset}
-  exit(1)
+  exit 1
 
-fi;
+fi
 
