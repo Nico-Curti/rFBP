b1742d6e10c1dfd5393617ee5d607a0507b2baea
Nico-Curti <nico.curti2@unibo.it>
2019-05-07 14:51:54 +0200

    minor fix for CI

--- scripts/download_atanherf.py
+++ scripts/download_atanherf.py
@@ -27,7 +27,7 @@ def download_file_from_google_drive(Id, destination, total_length = 230636758):
       for chunk in response.iter_content(chunk_size):
         dl += len(chunk)
         done = int(50 * dl / total_length)
-        sys.stdout.write('\r[%s%s] %3d%% (%1.1f Mb/sec) %3.0f sec' % ('█' * done,
+        sys.stdout.write('\r[%s%s] %3d%% (%1.1f Mb/sec) %3.0f sec' % ('o' * done,
                                                                    '-' * (50 - done),
                                                                    dl / total_length * 100,
                                                                    len(chunk) / 1000000 / (time.time() - download),
