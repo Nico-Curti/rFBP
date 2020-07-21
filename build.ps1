ce5c0a966d6a901f4923b9902bf01f2a4501ab77
Nico Curti <nico.curti2@unibo.it>
2020-07-21 12:33:36 +0200

    [REVISION] minor fix in windows compatibility and update scorer dependency

--- build.ps1
+++ build.ps1
@@ -4,18 +4,18 @@ $number_of_build_workers=8
 
 if ($null -eq (Get-Command "cl.exe" -ErrorAction SilentlyContinue)) {
   $vstype = "Professional"
-  if (Test-Path "C:\Program Files (x86)\Microsoft Visual Studio\2017\${vstype}\Common7\Tools") {
+  if (Test-Path "C:\Program Files (x86)\Microsoft Visual Studio\2019\${vstype}\Common7\Tools") {
   }
   else {
     $vstype = "Enterprise"
-    if (Test-Path "C:\Program Files (x86)\Microsoft Visual Studio\2017\${vstype}\Common7\Tools") {
+    if (Test-Path "C:\Program Files (x86)\Microsoft Visual Studio\2019\${vstype}\Common7\Tools") {
     }
     else {
       $vstype = "Community"
     }
   }
-  Write-Host "Found VS 2017 ${vstype}" -ForegroundColor Yellow
-  Push-Location "C:\Program Files (x86)\Microsoft Visual Studio\2017\${vstype}\Common7\Tools"
+  Write-Verbose "Found VS 2019 ${vstype}"
+  Push-Location "C:\Program Files (x86)\Microsoft Visual Studio\2019\${vstype}\Common7\Tools"
   cmd /c "VsDevCmd.bat -arch=x64 & set" |
     ForEach-Object {
     if ($_ -match "=") {
@@ -23,10 +23,10 @@ if ($null -eq (Get-Command "cl.exe" -ErrorAction SilentlyContinue)) {
     }
   }
   Pop-Location
-  Write-Host "Visual Studio 2017 ${vstype} Command Prompt variables set.`n" -ForegroundColor Yellow
+  Write-Verbose "Visual Studio 2019 ${vstype} Command Prompt variables set.`n" -ForegroundColor Yellow
 }
 else {
-  Write-Host "No Compiler found" -ForegroundColor Red
+  Write-Verbose "No Compiler found"
 }
 
 
@@ -34,7 +34,7 @@ else {
 Remove-Item .\build_win_debug -Force -Recurse -ErrorAction SilentlyContinue
 New-Item -Path .\build_win_debug -ItemType directory -Force
 Set-Location build_win_debug
-cmake -G "Visual Studio 15 2017" -T "host=x64" -A "x64" "-DCMAKE_BUILD_TYPE=Debug" ..
+cmake -G "Visual Studio 16 2019" -T "host=x64" -A "x64" "-DCMAKE_BUILD_TYPE=Debug" ..
 cmake --build . --config Debug --parallel ${number_of_build_workers} --target install
 Set-Location ..
 
@@ -42,7 +42,7 @@ Set-Location ..
 Remove-Item .\build_win_release -Force -Recurse -ErrorAction SilentlyContinue
 New-Item -Path .\build_win_release -ItemType directory -Force
 Set-Location build_win_release
-cmake -G "Visual Studio 15 2017" -T "host=x64" -A "x64" "-DCMAKE_BUILD_TYPE=Release" ..
+cmake -G "Visual Studio 16 2019" -T "host=x64" -A "x64" "-DCMAKE_BUILD_TYPE=Release" ..
 cmake --build . --config Release --parallel ${number_of_build_workers} --target install
 Set-Location ..
 
