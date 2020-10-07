a51c2722d7b8ded83cf8b897dcde475e916eb2d0
Nico-Curti <nico.curti2@unibo.it>
2020-10-07 16:34:54 +0200

    [REVISION] add scripts to data_files

--- setup.py
+++ setup.py
@@ -289,7 +289,8 @@ setup(
                                                          'ReplicatedFocusingBeliefPropagation.*'],
                                                 exclude=('test', 'example')),
   include_package_data          = True,
-  data_files                    = [('', ['CMakeLists.txt', 'README.md', 'LICENSE'])],
+  data_files                    = [('', ['CMakeLists.txt', 'README.md', 'LICENSE']),
+                                   ('scripts', [os.path.join(here, 'scripts', 'download_atanherf.py')])],
   platforms                     = 'any',
   classifiers                   = [
                                     # Full list: https://pypi.python.org/pypi?%3Aaction=list_classifiers
