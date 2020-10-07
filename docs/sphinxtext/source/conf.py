5ba43c210c752c94d7925f5ef3821df25a1033d4
Nico-Curti <nico.curti2@unibo.it>
2020-10-07 12:12:34 +0200

    [REVISION] working on docs CI

--- docs/sphinxtext/source/conf.py
+++ docs/sphinxtext/source/conf.py
@@ -24,6 +24,7 @@ author = "Nico Curti, Daniele Dall'Olio, Enrico Giampieri"
 # The full version, including alpha/beta/rc tags
 release = '1.0.1'
 
+master_doc = 'index'
 
 # -- General configuration ---------------------------------------------------
 
